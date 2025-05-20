55b26c3e6ad09425b43815add44c0ad69096fb05
sil <narradoresdigitales@gmail.com>
2025-05-20 04:37:48 -0500

    get all and get single function with students collection

--- controllers/students.js
+++ controllers/students.js
@@ -14,11 +14,15 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     //swagger.tags=['Contacts']
-    const userId = (req.params.id);
-    const student = await mongodb.getDatabase().db().collection('students').findOne({ _id: new ObjectId(userId) });
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('students').findOne({ _id: userId });
+    if (result) {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(student[0]);
-    };
+        res.status(200).json(result);
+    }else {
+        res.status(404).json({ message: `Student not found`});
+    }        
+};
 
 module.exports = {
     getAll,
