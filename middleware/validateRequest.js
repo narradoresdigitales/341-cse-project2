82066382845f5434a44a6431a1327e1f7744d4bb
sil <narradoresdigitales@gmail.com>
2025-05-22 11:27:17 -0500

    removed console.log to test validation

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,7 +1,6 @@
 const { validationResult } = require('express-validator');
 
 function validateRequest(req, res, next) {
-    console.log('validateRequest middleware running');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
