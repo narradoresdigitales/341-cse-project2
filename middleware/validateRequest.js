bf18a981e1b627d7e142d9407ba902d2eca88cc9
sil <narradoresdigitales@gmail.com>
2025-05-22 11:03:53 -0500

    added validation

--- middleware/validateRequest.js
+++ middleware/validateRequest.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
 function validateRequest(req, res, next) {
+    console.log('validateRequest middleware running');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
--- routes/courses.js
+++ routes/courses.js
@@ -2,15 +2,24 @@ const express = require('express');
 const router = express.Router();
 const coursesController = require('../controllers/courses');
 
+const {
+    createCourseValidationRules,
+    updateCourseValidationRules,
+    getCourseValidationRules,
+    deleteCourseValidationRules,
+} = require('../validations/courseValidation'); 
+
+const validateRequest = require('../middleware/validateRequest');
+
 
 router.get('/', coursesController.getAllCourses);
 
-router.get('/:id', coursesController.getCourse);
+router.get('/:id', getCourseValidationRules(),coursesController.getCourse);
 
-router.post('/', coursesController.createCourse);
+router.post('/', createCourseValidationRules(),coursesController.createCourse);
 
-router.put('/:id', coursesController.updateCourse);
+router.put('/:id', updateCourseValidationRules(),coursesController.updateCourse);
 
-router.delete('/:id', coursesController.deleteCourse);
+router.delete('/:id', deleteCourseValidationRules(),coursesController.deleteCourse);
 
 module.exports = router;
